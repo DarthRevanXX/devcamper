ca460a8f1e5c0d1100909540a766860178f5b333
Serhii Zadorozhnyi <serhii.zadorozhnyi@gmail.com>
2020-04-02 12:35:39 +0900

    added dev logger && middlewafe folder && controllers

---
+++ routes/bootcamps.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const {
+    getBootCamps,
+    getBootCamp,
+    createBootCamp,
+    deleteBootCamp,
+    updateBootCamp
+} = require('../controllers/bootcamps')
+const router = express.Router();
+
+router.route('/').get(getBootCamps).post(createBootCamp);
+router.route('/:id').get(getBootCamp).put(updateBootCamp).delete(deleteBootCamp);
+
+
+module.exports = router;
