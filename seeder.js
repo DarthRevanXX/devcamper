18981c20d2bdcfb55f325459095b50fb732ebd68
Serhii Zadorozhnyi <serhii.zadorozhnyi@rakuten.com>
2020-07-05 20:22:13 +0900

    added seeder to add or delete bootcamps

---
+++ seeder.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const colors = require('colors');
+const dotenv = require('dotenv');
+
+// Load env vars
+dotenv.config({
+    path: './config/config.env'
+})
+
+// Load Models
+const Bootcamp = require('./models/Bootcamp')
+
+// Connect to DB
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+});
+
+// Read the json files
+const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf8'));
+
+// Import into DB
+const importData = async () => {
+    try {
+        await Bootcamp.create(bootcamps)
+
+        console.log('Data imported successfully'.green.inverse);
+        process.exit();
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+// Delete data 
+const deleteData = async () => {
+    try {
+        await Bootcamp.deleteMany()
+
+        console.log('Data deleted successfully'.red.inverse);
+        process.exit();
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+if (process.argv[2] === '-i') {
+    importData();
+} else if (process.argv[2] === '-d') {
+    deleteData();
+}
