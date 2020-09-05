e52ae91f24d4072d2adc64b15399d4a0dfdcaa1f
Serhii Zadorozhnyi <serhii.zadorozhnyi@rakuten.com>
2020-09-05 14:49:39 +0900

    added course schema and added to seeder

--- seeder.js
+++ seeder.js
@@ -10,6 +10,7 @@ dotenv.config({
 
 // Load Models
 const Bootcamp = require('./models/Bootcamp')
+const Course = require('./models/Course')
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -21,12 +22,14 @@ mongoose.connect(process.env.MONGO_URI, {
 
 // Read the json files
 const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf8'));
+const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf8'));
+
 
 // Import into DB
 const importData = async () => {
     try {
         await Bootcamp.create(bootcamps)
-
+        await Course.create(courses)
         console.log('Data imported successfully'.green.inverse);
         process.exit();
     } catch (err) {
@@ -38,6 +41,7 @@ const importData = async () => {
 const deleteData = async () => {
     try {
         await Bootcamp.deleteMany()
+        await Course.deleteMany()
 
         console.log('Data deleted successfully'.red.inverse);
         process.exit();
