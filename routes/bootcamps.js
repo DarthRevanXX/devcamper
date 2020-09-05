5dff233ab57f32a6ece4d99d3e8252bb3a48f212
Serhii Zadorozhnyi <serhii.zadorozhnyi@rakuten.com>
2020-09-05 15:20:18 +0900

    added get all courses end-point and another

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -7,7 +7,14 @@ const {
     updateBootCamp,
     getBootcampsInRadius
 } = require('../controllers/bootcamps')
+
+// Include other ressource routers
+const courseRouter = require('./courses')
+
 const router = express.Router();
+
+// Re-routes into other resource routers
+router.use('/:bootcampId/courses', courseRouter)
 
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 router.route('/').get(getBootCamps).post(createBootCamp);
